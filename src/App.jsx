e139918951a5a941e0003378330004789c3ccebb
firoz.nur <firoznur5@gmail.com>
2025-04-28 23:16:28 +0600

    Change : Name & Design

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="underline">Vite + React</h1>
+      <h1 className="btn underline btn-primary items-center justify-center flex h-[50vh]">Rifat's Portfolio</h1>
+
     </>
   );
 }
