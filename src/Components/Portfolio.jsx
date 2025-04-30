af71f37695048ae55e3bac0c619540771abd0c06
firoz.nur <firoznur5@gmail.com>
2025-04-30 08:06:19 +0600

    Remove : Section From Portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -4,28 +4,6 @@ const Portfolio = () => {
   return (
     <div>
       <header className="bg-gradient-to-r from-[#fff4f3] to-[#f8e6da] pt-5 relative">
-        <section className="flex items-center relative justify-between max-w-6xl mx-auto pt-15">
-          <div>
-            <h1 className="text-6xl font-bold my-9">
-              Hi! I am
-              <br />
-              ROKIBUL HASAN RIFAT
-            </h1>
-            <p className="max-w-2xl text-gray-600">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Cupiditate tempora error autem magnam quia maxime omnis explicabo.
-              In, at sequi? Sed autem numquam officiis velit distinctio iure
-              quidem eius omnis!
-            </p>
-            <button className="btn btn-warning bg-[#e56813] text-white my-10">
-              Hire Me
-            </button>
-          </div>
-          <div>
-            <img className="w-64" src="/arafat-icon.png" alt="Arafat" />
-          </div>
-        </section>
-
         <div className="bg-white p-2 flex items-center gap-3 rounded-2xl w-fit absolute top-[82px] right-[84px]">
           <img
             className="w-16"
