27a2e2109e84e80bded943853ab9ee6af3fb7259
firoz.nur <firoznur5@gmail.com>
2025-04-30 07:30:29 +0600

    Fix : Icon Position & Make : Code Clean

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,27 +2,33 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between bg-white max-w-6xl mx-auto px-5 py-2 rounded-lg">
-      <div className="flex items-center gap-1">
-        <img className="w-16" src="/arafat-icon.png" alt="Arafat Icon" />
-      </div>
-      <div className="flex items-center gap-5">
-        <ul className="text-lg flex items-center gap-5">
-          <li className="font-bold">
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About Me</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-        </ul>
-        <button className="btn btn-warning bg-[#e56813] text-white">
-          Contact Me
-        </button>
-      </div>
-    </nav>
+    <header className="bg-gradient-to-r from-[#fff4f3] to-[#f8e6da] pt-5 relative">
+      <nav className="flex justify-between items-center max-w-6xl mx-auto px-5 py-2 bg-white rounded-lg">
+        <img
+          src="/arafat-icon.png"
+          alt="Arafat Icon"
+          className="size-16 object-cover object-top"
+        />
+        <div className="flex items-center gap-5">
+          <ul className="flex gap-5 text-lg font-medium">
+            <li>
+              <a href="#" className="font-bold">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">About Me</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+          </ul>
+          <button className="btn btn-warning bg-[#e56813] text-white">
+            Contact Me
+          </button>
+        </div>
+      </nav>
+    </header>
   );
 };
 
