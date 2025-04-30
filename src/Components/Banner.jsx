a400f415f0528a55dd3ed9458370653593f9c5a8
firoz.nur <firoznur5@gmail.com>
2025-04-30 08:04:06 +0600

    Fix : Name & Photo Position

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,26 +2,29 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <section className="flex items-center justify-between w-6xl mx-auto pt-15">
-      <div>
-        <h1 className="text-6xl font-bold my-9">
-          Hi! I am
-          <br />
-          Soyeb Ahmed Arafat
-        </h1>
-        <p className="w-2xl text-gray-600">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
-          tempora error autem magnam quia maxime omnis explicabo. In, at sequi?
-          Sed autem numquam officiis velit distinctio iure quidem eius omnis!
-        </p>
-        <button className="btn btn-warning bg-[#e56813] text-white my-10">
-          Hire Me
-        </button>
-      </div>
-      <div>
-        <img src="/arafat-icon.png" alt="" />
-      </div>
-    </section>
+    <header className="bg-gradient-to-r from-[#fff4f3] to-[#f8e6da] pt-5 relative">
+      <section className="flex justify-between items-center max-w-6xl mx-auto px-5 py-10">
+        <div>
+          <h1 className="text-6xl font-bold mb-6">
+            Hi! I am <br /> Rokibul Hasan Rifat
+          </h1>
+          <p className="max-w-2xl text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            tempora error autem magnam quia maxime omnis explicabo. In, at
+            sequi? Sed autem numquam officiis velit distinctio iure quidem eius
+            omnis!
+          </p>
+          <button className="btn btn-warning bg-[#e56813] text-white mt-8">
+            Hire Me
+          </button>
+        </div>
+        <img
+          src="/arafat-icon.png"
+          alt="Arafat Icon"
+          className="h-[587.76px]"
+        />
+      </section>
+    </header>
   );
 };
 
