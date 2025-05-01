34ee439d3e7e04ab6e7e983bc373b254600de642
firoz.nur <firoznur5@gmail.com>
2025-05-01 07:27:04 +0600

    Add : UI/UX Sticker

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -24,6 +24,19 @@ const Banner = () => {
           className="h-[587.76px]"
         />
       </section>
+      <div class="bg-white p-2 px-4 flex items-center gap-3 rounded-2xl w-fit absolute -bottom-12 right-175">
+        <img
+          class="w-16 bg-lime-100 p-2 rounded-full"
+          src="https://cdn3d.iconscout.com/3d/premium/thumb/ui-ux-3d-icon-download-in-png-blend-fbx-gltf-file-formats--web-design-development-programming-computer-graphic-pack-icons-9831982.png"
+          alt=""
+        />
+        <div>
+          <h3 class="text-lg font-bold">Ui/Ux</h3>
+          <p class="text-gray-700">
+            Lorem ipsum dolor sit <br /> amet consectetur
+          </p>
+        </div>
+      </div>
     </header>
   );
 };
