6c36c1d4a2449cfee2861b188e3b383bf4cf0a01
firoz.nur <firoznur5@gmail.com>
2025-05-03 06:33:03 +0600

    Add : Router Children

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,34 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Navbar from "./Components/Navbar.jsx";
+import Footer from "./Components/Footer.jsx";
+
+const Root = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/Certificates",
-    element: <div>Certificates</div>,
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <App></App>,
+      },
+      {
+        path: "/Certificates",
+        element: <div>Certificates</div>,
+      },
+    ],
   },
 ]);
 
