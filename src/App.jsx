30b4b027572f1fbae4a7c845e81b607b27b7da96
firoz.nur <firoznur5@gmail.com>
2025-05-03 06:36:49 +0600

    Remove : Duplicate Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
 import Banner from "./Components/Banner";
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
 import Portfolio from "./Components/Portfolio";
 
 function App() {
   return (
     <>
-      <Navbar></Navbar>
       <Banner></Banner>
       <Portfolio></Portfolio>
-      <Footer></Footer>
     </>
   );
 }
